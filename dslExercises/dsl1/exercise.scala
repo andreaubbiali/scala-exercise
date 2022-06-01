f9b2424a5d3dcb5175761a6185bff803f602dd86
aubbiali <aubbiali@sorint.it>
2022-06-01 15:20:18 +0200

    Added comments to make readable

--- dslExercises/dsl1/exercise.scala
+++ dslExercises/dsl1/exercise.scala
@@ -4,37 +4,42 @@ import scala.collection.mutable._
 
 class DeskCombinators(var the_table: HashMap[Char,Int])  extends JavaTokenParsers {
     
-    def program = "print" ~> expr ~ ("where" ~> rep1sep(decl, ",")) ^^
-                {
-                    case f ~ d => println(f());the_table
-                }
+    // split the string in things (expression and repetition of variable)
+    def program = "print" ~> expr ~ ("where" ~> rep1sep(decl, ",")) ^^ {
+            case f ~ d => f()
+        }
 
-    def expr: Parser[() => Int] = 
-        (
-            (x ~ ("+" ~> x)) ^^ 
-            {
-                case f1~f2 => () => f1() + f2()
-            }:Parser[()=>Int]
-        )
     /**
-    def program = "print" ~> expr ~ 
-            ( "where" ~> rep1sep(decl, ",")) ^^ { 
-                case f ~ d => println(f()); the_table 
-            }
-    
+     * parse the expression and return the function to exec this expression. There are 3 type of parse:
+     * 1. is something + expression
+     * 2. is a variable
+     * 3. is a number
+     * the 2 and 3 are needed to make the parse end. Without this two the parser need another expr and another and ...
+     * In case expression:  x+y+z+1+x+-3 the last value is a number so "num" is needed to finish the parse
+     * In case expression: x+y+z+1+x+y the last value is a var so "varToNum is needed to finish the parse"
+     */        
     def expr: Parser[() => Int] = 
         (
-            (( (num~("+" ~> expr))
-            | (x ~ ("+" ~> expr))
-        ) ^^ {case f1~f2 => () => f1() + f2()}:Parser[()=>Int])| x | num)
-    */
+            // 1. something(variable/number) "+" an expression
+            (
+                (( varToNum ~ ("+" ~> expr)) | (num ~ ("+" ~> expr))) ^^
+                    {
+                        case f1~f2 => () => f1() + f2()
+                    }:Parser[()=>Int]
+            ) 
+            // 2. return a function that cast a variable to a number
+            | varToNum 
+            // 3. return a number
+            | num 
+        )
 
-    def x = """[a-z]""".r ^^ {c => () => the_table(c.charAt(0))} 
+    // convert from variable to number
+    def varToNum = """[a-z]""".r ^^ {c => () => the_table(c.charAt(0))} 
     
+    // simply number
     def num = wholeNumber ^^ { n => () => n.toInt }
     
-    // qualcosa[a-z] ~ ("=" e wholeNumber)
-    // poi nel case vado a fare il vero parse e gli dico cosa fare
+    // add into the map the value of the variable
     def decl = """[a-z]""".r ~ ("=" ~> wholeNumber) ^^ {
         case c~n => the_table(c.charAt(0)) = n.toInt
     }
@@ -44,11 +49,11 @@ object DeskEvaluator {
     def main(args: Array[String]) = {
         val p = new DeskCombinators(new HashMap[Char,Int]())
         
-        // val lines = "print x+y+z+1+x+-3 where x = 25, y = 1, z=-7"
-        val semplice = "print x+y where x = 25, y = 1"
-
-        p.parseAll(p.program, semplice) match {
-            case p.Success(s,_) => println(s)
+        val lines = "print x+y+z+1+x+-3 where x = 25, y = 1, z=-7"
+        
+        // parse that return the result of the computation.
+        p.parseAll(p.program, lines) match {
+            case p.Success(s,_) => println("RESULT: "+ s)
             case x => print(x.toString)
         }
         

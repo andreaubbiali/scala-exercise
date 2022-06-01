33e5ac4d5207652ce0978978a65481a006fd432b
aubbiali <aubbiali@sorint.it>
2022-06-01 19:09:23 +0200

    exercise done

--- dslExercises/examRitirato/exercise.scala
+++ dslExercises/examRitirato/exercise.scala
@@ -1,76 +1,43 @@
 import scala.io.Source
 import scala.util.parsing.combinator.JavaTokenParsers
+import scala.collection.mutable
+import scala.util.matching.Regex
 
-class csvParser(var keys: List[String], var values: List[String], var index: Int) extends JavaTokenParsers{
 
-    def prova = firstLine <~ ("\n" ~> linee <~ """[\.]+""".r ) ^^ {
-        case a => values
+class csvParser(var mappa : mutable.HashMap[String, Array[String]]) extends JavaTokenParsers{
+
+    override val whiteSpace = """[ \t]+""".r
+
+    def start = line ~ ("\n" ~> rep1sep(line, "\n")) ^^{
+        case fstLine~values => (fstLine,values)
     }
 
-    def linee = repsep(linea, "\n") ^^ { 
-        case a => println(a) 
-    }
-
-    def linea = repsep(something, ",")  ^^ {
-        case a => a
-    }
-
-    
-
-  // def program: Parser[Program] = rep(command) ^^ { Program }
-
-
-    def firstLine = repsep(something, ",") ^^ {
-        case a => keys = a;
-    }
-
-    def restline: Unit = ((oneLine <~ "\n") | ".") ^^ {
-        case "." => println("FINEEEE")
-        case a => restline
-    }
-
-    def oneLine = repsep(something, "\n") ^^ {
-        case a => a
-    }
-
-/**
-    def restline = repsep(line, "\n") ^^ {
-        case a => println(a)
-    }
-
-    def line = ("." | repsep(something, ","))  ^^ {
-        case "." => println("FINEEEEEEE")
-        case a => println(a)
-    }
-*/
-    def something = ("""[\w\s\d\"\/]+""".r) ^^ {
-        case a => a.toString()
+    def line = rep1sep(word, ",") ^^ {
+        case l => l
     }
     
+    def word = """[a-zA-Z0-9\" /]+""".r ^^ {
+        case w => w
+    }
 }
 
 object main{
 
     def main(args: Array[String]): Unit = {
         
-       var lstKeys = List[String]() 
-       var lstValues = List[String]()
+       var mappa = new mutable.HashMap[String, Array[String]]()
 
-        val csv = new csvParser(lstKeys, lstValues, 0)
+        val csv = new csvParser(mappa)
 
         val src = scala.io.Source.fromFile("file.csv")
         val lines = src.mkString
         
-        csv.parseAll(csv.prova, lines) match{
-            case csv.Success(_, _) => printa(lstKeys)
+        csv.parseAll(csv.start, lines) match{
+            case csv.Success(res, _) => println(res._1)
+                                        println(res._2)
             case x => println(x.toString)
         }
 
         src.close()
-    }
-
-    def printa(values: List[String]) : Unit = {
-        println ("HA FINITO")
-    }
-    
+    }    
 }
